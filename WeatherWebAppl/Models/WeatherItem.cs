fc914399a276ffcd094276cb306c016791950fb5
nomoreframework <gleblukianenko@gmail.com>
2021-03-21 13:49:26 +0300

    change property

--- WeatherWebAppl/Models/WeatherItem.cs
+++ WeatherWebAppl/Models/WeatherItem.cs
@@ -8,7 +8,8 @@ namespace WeatherWebAppl.Models
         [JsonIgnore]
         public ObjectId Id { get; set; }
         public string Day { get; set; }
-        public string Temperature { get; set; }
+        public string MaxTemperature { get; set; }
+        public string MinTemperature { get; set; }
         public string WindSpeed { get; set; }
         public string Precipitation { get; set; }
         public string State { get; set; }
